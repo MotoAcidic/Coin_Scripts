789926cb658e29cbfc0da6201475421c0e11d621
TFinch <travisfinch01@gmail.com>
2018-08-02 13:17:36 -0400

    Update ccb_script.sh

--- CCBC/ccb_script.sh
+++ CCBC/ccb_script.sh
@@ -44,18 +44,18 @@ sudo apt-get install libqt4-dev libprotobuf-dev protobuf-compiler -y
 
 echo VPS Server prerequisites installed.
 echo Configuring server firewall.
-sudo ufw allow 45454
+sudo ufw allow 5520
 echo Server firewall configuration completed.
-echo Downloading AquilaX install files.
-wget https://github.com/aquilacoin/AquilaX/releases/download/V1.2.0.0/AquilaX-linux-Testnet.tar.gz
+echo Downloading CCBC install files.
+wget https://github.com/CryptoCashBack-Hub/CCBC/releases/download/V1.0.0.0/CCBC-linux.tar.gz
 echo Download complete.
-echo Installing AquilaX.
-tar xvf AquilaX-linux-Testnet.tar.gz
-chmod 775 ./Aquilad
-chmod 775 ./Aquila-cli
-echo AquilaX install complete. 
+echo Installing CCBC.
+tar xvf CCBC-linux.tar.gz
+chmod 775 ./cryptocashbackd
+chmod 775 ./cryptocashback-cli
+echo CCBC install complete. 
 
-echo Now ready to setup AquilaX configuration file.
+echo Now ready to setup CCBC configuration file.
 
 
 RPCUSER=NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
@@ -64,9 +64,9 @@ VPSIP=$(curl -s4 icanhazip.com)
 echo Please input your private key.
 read GENKEY
 
-mkdir -p /root/.Aquila && touch /root/.Aquila/Aquila.conf
+mkdir -p /root/.Aquila && touch /root/.cryptocashback/cryptocashback.conf
 
-cat > /root/.Aquila/Aquila.conf << EOF
+cat > /root/.cryptocashback/cryptocashback.conf << EOF
 rpcuser=$RPCUSER
 rpcpassword=$RPCPASSWORD
 rpcallowip=127.0.0.1
@@ -75,8 +75,8 @@ listen=1
 daemon=1
 staking=1
 rpcallowip=127.0.0.1
-rpcport=45455
-port=19333
+rpcport=5522
+port=5520
 logtimestamps=1
 maxconnections=256
 masternode=1
@@ -88,9 +88,9 @@ addnode=140.82.43.229:19333
 addnode=104.238.131.253:19333
 EOF
 
-echo AquilaX configuration file created successfully. 
-echo Please start your new AquilaX masternode by running ./Aquilad -daemon
-echo If you get a message asking to rebuild the database, please hit Ctr + C and run ./Aquilad -daemon -reindex
+echo CCBC configuration file created successfully. 
+echo Please start your new CCBC masternode by running ./cryptocashbackd -daemon
+echo If you get a message asking to rebuild the database, please hit Ctr + C and run ./cryptocashbackd -daemon -reindex
 echo If you still have further issues please reach out to support in our Discord channel. 
             ;;
         2)
@@ -100,9 +100,9 @@ VPSIP=$(curl -s4 icanhazip.com)
 echo Please input your private key.
 read GENKEY
 
-mkdir -p /root/.Aquila && touch /root/.Aquila/Aquila.conf
+mkdir -p /root/.Aquila && touch /root/.cryptocashback/cryptocashback.conf
 
-cat > /root/.Aquila/Aquila.conf << EOF
+cat > /root/.cryptocashback/cryptocashback.conf << EOF
 rpcuser=$RPCUSER
 rpcpassword=$RPCPASSWORD
 rpcallowip=127.0.0.1
@@ -111,35 +111,35 @@ listen=1
 daemon=1
 staking=1
 rpcallowip=127.0.0.1
-rpcport=45455
-port=19333
+rpcport=5522
+port=5520
 logtimestamps=1
 maxconnections=256
 masternode=1
 externalip=$VPSIP
 masternodeprivkey=$GENKEY
-addnode=144.202.54.65:19333
-addnode=45.32.200.48:19333
-addnode=140.82.43.229:19333
-addnode=104.238.131.253:19333
+addnode=144.202.54.65:5520
+addnode=45.32.200.48:5520
+addnode=140.82.43.229:5520
+addnode=104.238.131.253:5520
 EOF
 
-echo AquilaX configuration file has been updated successfully.
-echo You can start your new AquilaX masternode manually by running ./Aquilad -testnet
+echo CCBC configuration file has been updated successfully.
+echo You can start your new AquilaX masternode manually by running ././cryptocashbackd -testnet
 echo If you get a message asking to rebuild the database, please hit Ctr + C and run ./Aquilad -daemon -reindex
 echo If you still have further issues please reach out to support in our Discord channel.
             ;;
         3)
-            ./Aquilad -daemon
-		echo "If you get a message asking to rebuild the database, please hit Ctr + C and rebuild Aquila Index. (Option 5)"
+            ./cryptocashbackd -daemon
+		echo "If you get a message asking to rebuild the database, please hit Ctr + C and rebuild cryptocashback Index. (Option 5)"
             ;;
 	4)
-            ./Aquila-cli stop
+            ./cryptocashback-cli stop
             ;;
 	5)
-           ./Aquilad -daemon -reindex
+           ./cryptocashbackd -daemon -reindex
             ;;
         6)
-	   ./Aquila-cli getinfo
+	   ./cryptocashback-cli getinfo
 	   ;;
 esac
