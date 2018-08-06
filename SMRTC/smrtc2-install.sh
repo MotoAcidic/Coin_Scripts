e3d30e3c68b411cbe412829eab5381b8d998d0af
TFinch <travisfinch01@gmail.com>
2018-08-06 19:14:38 -0400

    Update smrtc2-install.sh

--- SMRTC/smrtc2-install.sh
+++ SMRTC/smrtc2-install.sh
@@ -1,183 +1,54 @@
 #!/bin/bash
-
-HEIGHT=15
-WIDTH=40
-CHOICE_HEIGHT=6
-BACKTITLE="CCB Masternode Setup Wizard"
-TITLE="CCB VPS Setup"
-MENU="Choose one of the following options:"
-
-OPTIONS=(1 "Install New VPS Server"
-         2 "Update to new version VPS Server"
-         3 "Start CCB Masternode"
-	 4 "Stop CCB Masternode"
-	 5 "CCB Server Status"
-	 6 "Rebuild CCB Masternode Index")
-
-
-CHOICE=$(whiptail --clear\
-		--backtitle "$BACKTITLE" \
-                --title "$TITLE" \
-                --menu "$MENU" \
-                $HEIGHT $WIDTH $CHOICE_HEIGHT \
-                "${OPTIONS[@]}" \
-                2>&1 >/dev/tty)
-
-clear
-case $CHOICE in
-        1)
-            echo Starting the install process.
-echo Checking and installing VPS server prerequisites. Please wait.
-echo -e "Checking if swap space is needed."
-PHYMEM=$(free -g|awk '/^Mem:/{print $2}')
-SWAP=$(swapon -s)
-if [[ "$PHYMEM" -lt "2" && -z "$SWAP" ]];
-  then
-    echo -e "${GREEN}Server is running with less than 2G of RAM, creating 2G swap file.${NC}"
-    dd if=/dev/zero of=/swapfile bs=1024 count=2M
-    chmod 600 /swapfile
-    mkswap /swapfile
-    swapon -a /swapfile
-else
-  echo -e "${GREEN}The server running with at least 2G of RAM, or SWAP exists.${NC}"
-fi
-if [[ $(lsb_release -d) != *16.04* ]]; then
-  echo -e "${RED}You are not running Ubuntu 16.04. Installation is cancelled.${NC}"
-  exit 1
-fi
-
-if [[ $EUID -ne 0 ]]; then
-   echo -e "${RED}$0 must be run as root.${NC}"
-   exit 1
-fi
-clear
-sudo apt update
-sudo apt-get -y upgrade
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
 sudo apt-get install git -y
-sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils -y
-sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev -y
-sudo apt-get install libboost-all-dev -y
-sudo apt-get install software-properties-common -y
-sudo add-apt-repository ppa:bitcoin/bitcoin -y
-sudo apt-get update
-sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
-sudo apt-get install libminiupnpc-dev -y
+sudo apt-get install nano -y
+sudo apt-get install curl -y
+sudo apt-get install pwgen -y
+sudo apt-get install wget -y
+sudo apt-get install build-essential libtool automake autoconf -y
+sudo apt-get install autotools-dev autoconf pkg-config libssl-dev -y
+sudo apt-get install libgmp3-dev libevent-dev bsdmainutils libboost-all-dev -y
 sudo apt-get install libzmq3-dev -y
-sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler -y
-sudo apt-get install libqt4-dev libprotobuf-dev protobuf-compiler -y
-clear
-echo VPS Server prerequisites installed.
+sudo apt-get install libminiupnpc-dev -y
+sudo add-apt-repository ppa:bitcoin/bitcoin -y
+sudo apt-get update -y
+sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
 
+cd
+#remove old files
+rm smrtc*
 
-echo Configuring server firewall.
-sudo apt-get install -y ufw
-sudo ufw allow 19551
-sudo ufw allow ssh/tcp
-sudo ufw limit ssh/tcp
-sudo ufw logging on
-echo "y" | sudo ufw enable
-sudo ufw status
-echo Server firewall configuration completed.
-
-echo Downloading AquilaX install files.
-wget https://github.com/SMRT-Cloud/smrtc/releases/download/v1.0.0.0/smrtc-linux.tar.gz
-echo Download complete.
-
-echo Installing smrtc.
+#get wallet files
+#rm smrtc-linux.tar.gz
+wget https://github.com/telostia/smartcloud-guides/raw/master/wallet/linux64/smrtc-linux.tar.gz
 tar -xvf smrtc-linux.tar.gz
-chmod 775 ./smrtcd
-chmod 775 ./smrtc-cli
-echo smrtc install complete. 
-sudo rm -rf smrtc-linux.tar.gz
-clear
+rm smrtc-linux.tar.gz
+chmod +x smrtc*
+#cp smrtc* /usr/local/bin
+rm smartc_auto.sh
+#rm smrtc*
+ufw allow 9887/tcp
 
-echo Now ready to setup AquilaX configuration file.
+#masternode input
 
-RPCUSER=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-RPCPASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+echo -e "${GREEN}Now paste your Masternode key by using right mouse click ${NONE}";
+read MNKEY
+
 EXTIP=`curl -s4 icanhazip.com`
-echo Please input your private key.
-read GENKEY
+USER=`pwgen -1 20 -n`
+PASSW=`pwgen -1 20 -n`
 
-mkdir -p /root/.smrtc && touch /root/.smrtc/smrtc.conf
+echo -e "${GREEN}Preparing config file ${NONE}";
+sudo mkdir $HOME/.smrtc
 
-cat << EOF > /root/.smrtc/smrtc.conf
-rpcuser=$RPCUSER
-rpcpassword=$RPCPASSWORD
-rpcallowip=127.0.0.1
-server=1
-listen=1
-daemon=1
-staking=1
-rpcallowip=127.0.0.1
-rpcport=19112
-port=9887
-logtimestamps=1
-maxconnections=256
-masternode=1
-externalip=$EXTIP
-masternodeprivkey=$GENKEY
-addnode=108.224.49.202:9887
-addnode=107.172.249.143:9887
-addnode=23.94.183.5:9887
-addnode=172.245.6.154:9887
-EOF
-clear
-./smrtcd -daemon
-./smrtc-cli stop
-./smrtcd -daemon
-clear
-echo cryptocashback configuration file created successfully. 
-echo cryptocashback Server Started Successfully using the command ./smrtcd -daemon
-echo If you get a message asking to rebuild the database, please hit Ctr + C and run ./smrtcd -daemon -reindex
-echo If you still have further issues please reach out to support in our Discord channel. 
-echo Please use the following Private Key when setting up your wallet: $GENKEY
-            ;;
-	    
-    
-        2)
-sudo ./smrtc-cli -daemon stop
-echo "! Stopping smrtc Daemon !"
+printf "addnode=139.99.197.135\n
+addnode=108.224.49.202\n
+addnode=107.172.249.143\n
+addnode=23.94.183.5\n
+addnode=172.245.6.154\n
+\nrpcuser=smartcuser$USER\nrpcpassword=$PASSW\ndaemon=1\nlisten=1" >  $HOME/.smrtc/smrtc.conf
 
-echo Configuring server firewall.
-sudo apt-get install -y ufw
-sudo ufw allow 9887
-sudo ufw allow ssh/tcp
-sudo ufw limit ssh/tcp
-sudo ufw logging on
-echo "y" | sudo ufw enable
-sudo ufw status
-echo Server firewall configuration completed.
-
-echo "! Removing CCB !"
-sudo rm -rf smrtc-linux.tar.gz
-sudo rm -rf smrtcd
-sudo rm -rf smrtc-cli
-sudo rm -rf smrtc-qt
-
-
-
-wget https://github.com/SMRT-Cloud/smrtc/releases/download/v1.0.0.0/smrtc-linux.tar.gz
-echo Download complete.
-echo Installing CCB.
-tar -xvf smrtc-linux.tar.gz
-chmod 775 ./smrtcd
-chmod 775 ./smrtc-cli
-echo AquilaX install complete. 
-sudo rm -rf smrtc-linux.tar.gz
-
-            ;;
-        3)
-            ./smrtcd -daemon
-		echo "If you get a message asking to rebuild the database, please hit Ctr + C and rebuild Aquila Index. (Option 6)"
-            ;;
-	4)
-            ./smrtc-cli stop
-            ;;
-	5)
-	    ./smrtc-cli getinfo
-	    ;;
-        6)
-	     ./smrtc -daemon -reindex
-            ;;
-esac
+./smrtcd
+watch ./smrtc-cli getinfo
