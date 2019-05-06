e80f6806ab6dd58bf4a9633bd36ba72032ffc2b1
ThatGuy <travisfinch01@gmail.com>
2019-05-06 14:13:43 -0400

    Update abet_install.sh

--- Abet/abet_install.sh
+++ Abet/abet_install.sh
@@ -16,7 +16,8 @@ OPTIONS=(1 "Install New VPS Server"
 	 6 "Stop Abet Masternode"
 	 7 "Abet Server Status"
 	 8 "Rebuild Abet Masternode Index"
-	 9 "Start Testnet")
+	 9 "Start Testnet"
+	 10 "Install Depends")
 
 
 CHOICE=$(whiptail --clear\
@@ -391,4 +392,20 @@ echo ABET install complete.
             ;;
 	9)   ./altbetd -testnet
 	    ;;
+	10)  
+	     sudo apt update
+sudo apt-get -y upgrade
+sudo apt-get install git -y
+sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils -y
+sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev -y
+sudo apt-get install libboost-all-dev -y
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:bitcoin/bitcoin -y
+sudo apt-get update
+sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
+sudo apt-get install libminiupnpc-dev -y
+sudo apt-get install libzmq3-dev -y
+sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler -y
+sudo apt-get install libqt4-dev libprotobuf-dev protobuf-compiler -y
+	     ;;
 esac
